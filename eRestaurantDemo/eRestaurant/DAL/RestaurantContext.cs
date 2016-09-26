922806dcd88ef77fb7aca044391631b0bf3aa8d1
dsingh26 <dgrewal91@gmail.com>
2016-09-26 16:36:42 -0600

    added entites and web config settings

---
+++ eRestaurantDemo/eRestaurant/DAL/RestaurantContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using eRestaurant.Entities;
+
+
+namespace eRestaurant.DAL
+{
+    class RestaurantContext: DbContext
+    {
+        public RestaurantContext(): base("name=EatIn") { }
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Reservation>Reservation { get; set; }
+        public DbSet<MenuCategory>MenuCategories { get; set; }
+        public DbSet<Item>Item { get; set; }
+    }
+}
